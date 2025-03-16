e0be248499d891c2fba4980b390255c149c6d477
Pavan-2507 <surlapavankumar94@gmail.com>
2025-03-16 20:39:20 +0530

    add some functionalities from to js file

--- boolean.js
+++ boolean.js
@@ -2,6 +2,12 @@ const choices=["rock", "paper", "scissors" ];
 const playerDispaly=document.getElementById("playerDisplay");
 const computerDisplay=document.getElementById("computerDisplay");
 const resultDisplay=document.getElementById("resultDisplay");
+const playerScore=document.getElementById("playerScore");
+ const computerScore=document.getElementById("computerScore");
+ const draw=document.getElementById("draw");
+   let myscore=0;
+   let computer=0;
+   let drawc=0;
 
 
 function playgame(playerchoice){
@@ -27,10 +33,36 @@ function playgame(playerchoice){
             break;       
     }
   }
+
+  if(result=== "YOU WIN!"){
+    myscore=myscore+1;
+  }
+  
+    else if(result=== "YOU LOSE!"){
+     computer=computer+1;
+  }else{
+    drawc=drawc+1;
+  }
   console.log(playerchoice);
   console.log(computerchoice);
    playerDispaly.textContent=`PLAYER: ${playerchoice}`;
    computerDisplay.textContent=`COMPUTER: ${computerchoice}`;
   resultDisplay.textContent=result;
   console.log(result);
+     playerScore.textContent=`YOU:${myscore}`;
+    computerScore.textContent=`COMPUTER:${computer}`;
+    draw.textContent=`TIE:${drawc}`;
   }
+
+function resetgame(){
+  myscore = 0;
+  computer = 0;
+  drawc = 0;
+
+  playerDisplay.textContent = "PLAYER :";
+  computerDisplay.textContent = "COMPUTER :";
+  resultDisplay.textContent = "";
+  playerScore.textContent = "YOU:0";
+  computerScore.textContent = "COMPUTER:0";
+  draw.textContent = "TIE:0";
+}
